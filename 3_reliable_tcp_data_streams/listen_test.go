8500c8859b498cc0902fb815385e83fa3e75ba57
Joshua Tsang <joshua.tsang@soundmouse.com>
2022-02-06 19:19:59 +0000

    added listener

--- 3_reliable_tcp_data_streams/listen_test.go
+++ 3_reliable_tcp_data_streams/listen_test.go
@@ -5,3 +5,12 @@ import (
 	"testing"
 )
 
+func TestListener(t *testing.T) {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = listener.Close() }()
+
+	t.Logf("Bound to %q", listener.Addr())
+}
